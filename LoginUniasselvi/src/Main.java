e50e7c4d42ea871f57be36c6ff987987600a481a
Jlnoris <jlnoris@gmail.com>
2020-03-30 21:38:07 -0300

    Insercao de arquivos Eclipse

---
+++ LoginUniasselvi/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		PessoaFisica pessoa1Pessoa = new PessoaFisica();
+		pessoa1Pessoa.setNome( "João Noris" );
+		pessoa1Pessoa.setCpf("89452610");
+		/**/
+		System.out.print(pessoa1Pessoa.getNome() );
+	}
+
+}
